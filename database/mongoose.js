8c2df3eca2c7cb5f8ddfe9d2c4f3d62c0affd39e
neriosoft <alabaneri@gmail.com>
2022-03-22 01:04:30 +0100

    Added express and mongoose configurations

---
+++ database/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb', { useNewUrlParser:true, useunifiedTopology:true })
+.then(()=> console.log("DB Connected Successful"))
+.catch((error)=> {
+    console.log(error)});
+
+module.exports = mongoose;
