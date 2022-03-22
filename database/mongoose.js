8b9ea85e5a87019c51e00a4fa9c44f42f0e8060a
neriosoft <alabaneri@gmail.com>
2022-03-22 01:22:25 +0100

    implemented message to be displayed for error in db connection

--- database/mongoose.js
+++ database/mongoose.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb', { useNewUrlParser:true, useunifiedTopology:true })
 .then(()=> console.log("DB Connected Successful"))
 .catch((error)=> {
-    console.log(error)});
+    console.log("Error occurred while connecting to DB",error)});
 
 module.exports = mongoose;
